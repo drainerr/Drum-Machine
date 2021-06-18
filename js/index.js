83ca4ca8c48b116bdd8088c285f672096718046e
Georgeave <67703102+Georgeave@users.noreply.github.com>
2021-06-18 21:20:36 +0400

    fix cases in switch operator

--- js/index.js
+++ js/index.js
@@ -15,14 +15,23 @@ window.document.onkeyup = (e) =>{
     var key = e.key;
     switch(key){
         case 'q' : play('Qa'); break;
+        case 'Q' : play('Qa'); break;
         case 'w' : play('Wa'); break;
+        case 'W' : play('Wa'); break;
         case 'e' : play('Ea'); break;
+        case 'E' : play('Ea'); break;
         case 'a' : play('Aa'); break;
+        case 'A' : play('Aa'); break;
         case 's' : play('Sa'); break;
+        case 'S' : play('Sa'); break;
         case 'd' : play('Da'); break;
+        case 'D' : play('Da'); break;
         case 'z' : play('Za'); break;
+        case 'Z' : play('Za'); break;
         case 'x' : play('Xa'); break;
+        case 'X' : play('Xa'); break;
         case 'c' : play('Ca'); break;
+        case 'C' : play('Ca'); break;
     }
 }
 
